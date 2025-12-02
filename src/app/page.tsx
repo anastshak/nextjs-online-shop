0523ef612b1c2c72c47cf67303904309f1ae5a19
anastshak <nastyashakura@gmail.com>
2025-12-02 20:57:12 +0300

    chore: delete unnecessary dep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Spinner } from '@/components/ui/spinner';
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      hello <Spinner className="size-6 text-green-500" />
+      hello <Spinner className="size-10 text-green-600" />
     </div>
   );
 }
